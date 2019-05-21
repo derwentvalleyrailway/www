ce1a75e655c57f5a6d3aae09a78617a25df60c53
Owen Andrews <owen@owenandre.ws>
2019-05-21 21:53:46 +1000

    Converted shop

--- content/shop/_index.md
+++ content/shop/_index.md
@@ -3,3 +3,7 @@ title = "Shop"
 menu = "main"
 weight = 3
 +++
+
+{{% container %}}
+Help us fund the ongoing restoration and maintenance of rolling-stock and rail infrastructure by purchasing items from our online shop.
+{{% /container %}}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,6 @@ module.exports = {
     variants: {
         backgroundColor: ['hover', 'group-hover'],
         display: ['responsive', 'group-hover'],
+        textDecoration: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
     },
 }
